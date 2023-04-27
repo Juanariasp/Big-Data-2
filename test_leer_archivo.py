8bb7e99ae838b837a86af77eeeaf3dae7e3f5a40
Ubuntu <ubuntu@ip-172-31-2-216.ec2.internal>
2023-04-27 20:40:23 +0000

    prueba final

--- test_leer_archivo.py
+++ test_leer_archivo.py
@@ -3,14 +3,6 @@ from datetime import datetime
 from leer_archivo import get_url_tiempo, get_boto, get_date, get_url_publimetro
 
 
-def test_get_boto(mocker):
-    mocker.patch("boto3.client")
-    s3_mock = mocker.MagicMock()
-    boto3.client.return_value = s3_mock
-    s3 = get_boto()
-    assert s3 == s3_mock
-
-
 def test_get_date():
     dt = get_date()
     assert dt == datetime.today().strftime('%Y-%m-%d')
